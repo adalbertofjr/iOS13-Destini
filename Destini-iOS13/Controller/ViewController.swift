3b204f4943a3932b90187f077f749ef31e16e9cf
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2021-03-18 23:44:32 -0300

    Escolhendo seu destino

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var storyBrain = StoryBrain()
+    
+    // MARK: IBOutlets
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    // MARK: IBActions
+    @IBAction func choiceMade(_ sender: UIButton) {
+        guard let choice = sender.currentTitle else {
+            return
+        }        
+        storyBrain.nextStory(choice)
+        updateUI()
+    }
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
-
-
+    
+    //MARK: Functions
+    func updateUI(){
+        let story = storyBrain.getStory()
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
 }
 
